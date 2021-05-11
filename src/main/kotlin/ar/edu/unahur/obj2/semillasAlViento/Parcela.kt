45d314d64fc2fd4227bd8dbabf0c10c25fb08ab8
Lucho G <goncalvesdacosta.l@gmail.com>
2021-05-11 20:40:11 -0300

    terminado parcela

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Parcela.kt
@@ -2,14 +2,19 @@ package ar.edu.unahur.obj2.semillasAlViento
 
 class Parcela(val ancho: Int, val largo: Int, val horasSolPorDia: Int) {
   val plantas = mutableListOf<Planta>()
+  //Simplicidad KISS, no es necesaro cantidadPlantas.
   var cantidadPlantas = 0
 
   fun superficie() = ancho * largo
   fun cantidadMaximaPlantas() =
+    //Redundancia mínima en ancho * largo pudiendo usar superficie() / 5.
     if (ancho > largo) ancho * largo / 5 else ancho * largo / 3 + largo
 
   fun plantar(planta: Planta) {
+    //Se podía usar el tamaño de la lista de la planta y comparar si es mayor o igual a la cantidad de plantas que tolera
+    //Redundancia mínima
     if (cantidadPlantas == this.cantidadMaximaPlantas()) {
+      //No pide un comentario sino que tire un error, robustez
       println("Ya no hay lugar en esta parcela")
     } else if (horasSolPorDia > planta.horasDeSolQueTolera() + 2) {
       println("No se puede plantar esto acá, se va a quemar")
