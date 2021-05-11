37138c503134267f81c8f0e1cf45da7ba44ac8f9
Lucho G <goncalvesdacosta.l@gmail.com>
2021-05-11 19:33:31 -0300

    terminado comentario en planta

--- src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/semillasAlViento/Planta.kt
@@ -3,6 +3,8 @@ package ar.edu.unahur.obj2.semillasAlViento
 abstract class Planta(val anioObtencionSemilla: Int, var altura: Float) {
   fun esFuerte() = this.horasDeSolQueTolera() > 10
 
+  //No se usa bien la abstracción
+  //parcelaTieneComplicaciones() tiene que ir en parcela, no en planta
   fun parcelaTieneComplicaciones(parcela: Parcela) =
     parcela.plantas.any { it.horasDeSolQueTolera() < parcela.horasSolPorDia }
 
@@ -15,7 +17,9 @@ class Menta(anioObtencionSemilla: Int, altura: Float) : Planta(anioObtencionSemi
   override fun daSemillas() = this.esFuerte() || altura > 0.4
 }
 
+//No se pide saber si es o no transgenica, está demás. Simplicidad, YAGNI
 class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean) : Planta(anioObtencionSemilla, altura) {
+
   override fun horasDeSolQueTolera(): Int  {
     // ¡Magia de Kotlin! El `when` es como un `if` pero más poderoso:
     // evalúa cada línea en orden y devuelve lo que está después de la flecha.
@@ -25,11 +29,13 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
       else          -> 9
     }
 
+    //YAGNI
     return if (esTransgenica) horasBase * 2 else horasBase
   }
 
 
   override fun daSemillas(): Boolean  {
+    //YAGNI
     if (this.esTransgenica) {
       return false
     }
@@ -37,3 +43,5 @@ class Soja(anioObtencionSemilla: Int, altura: Float, val esTransgenica: Boolean)
     return this.esFuerte() || (this.anioObtencionSemilla > 2007 && this.altura > 1)
   }
 }
+
+//Falta clase soja transgenica que hereda de soja. Mutaciones controladas
